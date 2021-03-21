bb1a9c2d36ee9717c8192e11c61a2d766d450d85
Federico Boulocq <fboulocq@prodtech.com.ar>
2021-03-21 20:58:41 -0300

    Clase 10

---
+++ Clase 10/script.js
@@ -0,0 +1,25 @@
+// let input = document.getElementById("file");
+// let div = document.getElementById("idDiv");
+// let fileImg = document.getElementById("fileImg");
+// input.addEventListener("change", (e) =>{
+//     e.preventDefault();
+//     e.stopPropagation();
+
+//     let archivo = e.target.files[0];
+//     let reader = new FileReader();
+//     reader.addEventListener("load", (e) =>{
+//         console.log(e.target);
+//         fileImg.setAttribute("src", e.target.result)
+//     })
+
+
+//     console.log(archivo);
+    
+// })
+
+function externa()
+{
+    let inetrna = function(){
+
+    };
+}
